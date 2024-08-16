fff56f444000e48808c4352354843d5a6927b41d
aditya200523 <mahajan.aaditya2020@gmail.com>
2024-08-16 17:29:15 +0530

    Updated Program counter for the interrupt

--- rtl/pc.v
+++ rtl/pc.v
@@ -13,25 +13,45 @@
  * @return out_sign Current value of the program counter.
  */
 
+
+//The address of the interrupt is stored in the mtvec 
+//mret is the signal confirming return from interrupt 
+//mepc is the one where the address after interrupt is stored
+
+
+
+
 module PC(
-   input clk,
-   input reset,
-   input j_signal,
-   input [31:0] jump,
-   output[31:0] out_sign
+    input clk,
+    input reset,
+    input j_signal,
+    input [31:0] jump,
+    input trap_detected,       	   // Signal indicating an interrupt 
+    input [31:0] mtvec,            // Interrupt vector base address
+    input mret,                    // Signal for returning from interrupt
+    input [31:0] mepc,             // Address to return to after interrupt
+    
+    output reg [31:0] out_sign     // Program Counter output
 );
-   reg [31:0] next_pc = 32'd0;
-   
-   always @ (posedge clk) begin
-		if(reset)
-			next_pc <= 32'b0;
-		else if(j_signal) begin
-			next_pc <= jump;
-		end
-		else begin
-			next_pc <= next_pc + 32'd4;
-		end
-   end
-	
-	assign out_sign = next_pc;
-endmodule 
+    reg [31:0] next_pc = 32'd0;
+
+    always @ (posedge clk) begin
+        if (reset) begin
+            next_pc <= 32'b0;
+        end else if (trap_detected) begin
+            next_pc <= mtvec;        // Redirect PC to interrupt handler
+        end else if (mret) begin
+            next_pc <= mepc;         // Return from interrupt
+        end else if (j_signal) begin
+            next_pc <= jump;         // Handle jump instructions
+        end else begin
+            next_pc <= next_pc + 32'h4; // Increment PC for normal execution
+        end
+    end
+
+    always @ (posedge clk) begin
+        if (!reset) begin
+            out_sign <= next_pc;    // Output the current PC value
+        end
+    end
+endmodule
